31f553657ed27ec7823669a1378c1d476a5c6be5
Mitch Willis <mitchellwillis33@gmail.com>
2024-12-06 16:23:33 +1300

    update

--- app.py
+++ app.py
@@ -94,6 +94,11 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/maintenance')
+def maintenance():
+    return render_template("maintenance.html")
+
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'GET':
